48f64ddfcc33fb058f053e15c80331b56fd560da
Ocean Sun <sunsjyvip@vip.qq.com>
2022-03-22 14:47:43 +0800

    Initialize the C# solution

---
+++ csharpsrc/Tester/Program.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Program.cs" company="Ocean">
+// Copyright (c) Ocean. All rights reserved.
+// </copyright>
+
+namespace Tester
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
