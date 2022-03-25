441f1c2ed6080f61aa764f3e5aaad6b773f0bd5c
Ocean Sun <sunsjyvip@vip.qq.com>
2022-03-25 19:22:08 +0800

    Add LeetCode No.18 question: Four Sum.
    And adjust the README.md file.file.
    Add CHANGELOG.md file.

---
+++ csharpsrc/Questions/LeetCode/No18.FourSum/IQuestion.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IQuestion.cs" company="Ocean">
+// Copyright (c) Ocean. All rights reserved.
+// </copyright>
+
+namespace Questions.LeetCode.No18.FourSum
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The LeetCode No.18 question: Four Sum.
+    /// </summary>
+    public interface IQuestion
+    {
+        /// <summary>
+        /// Gets all unique quadruplets from numbers array and their sum need to equals to target.
+        /// </summary>
+        /// <param name="nums">The numbers array.</param>
+        /// <param name="target">The target.</param>
+        /// <returns>All unique quadruples.</returns>
+        IList<IList<int>> FourSum(int[] nums, int target);
+    }
+}
